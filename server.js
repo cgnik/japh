4f1b61dec54241f45334c8842907d207a75feaf4
_cking <cking@enmobile.biz>
2018-03-30 13:15:57 -0400

    added some simple math operations

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+const add = nums => (nums || []).reduce((accum, val) => accum + val, 0);
+const subtract = nums => (nums || []).reduce((accum, val) => accum - val, 0);
+
+const operations = {
+   add: add,
+   subtract: subtract
+};
+const validate = (oper,data) => oper in operations && Array.isArray(data);
+const calculate = (oper, data) => ({answer: operations[oper](data)});
+const analyze = (oper, data) => validate(oper, data) ? calculate(oper, data) : {error: "Invalid request"};
+
 app.listen(port);
 app.use(express.static('static'));
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
 app.route('/analysis')
    .get((req, resp) => resp.json([
-      {name: "Add", description: "sum the values"},
-      {name: "Subtract", description: "subtract the values from first to last"}
+      {id: "add", name: "Add", description: "sum the values"},
+      {id: "subtract", name: "Subtract", description: "subtract the values from first to last"}
    ]));
+app.route('/analysis/:operation')
+   .post((req, resp) => resp.json(analyze(req.params.operation, req.body)));
 
